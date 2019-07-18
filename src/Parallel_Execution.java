66c01c67172ea5b83e4a71d9d283f2bce5fa6f4a
107925 <107925@merrillcorp.com>
2019-07-18 17:08:28 +0530

    created few files

---
+++ src/Parallel_Execution.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+
+
+
+
+public class Parallel_Execution {
+	
+	public void parallelExecution() {
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+	
+	
+
+}}
